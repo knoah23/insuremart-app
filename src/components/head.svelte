3885cccdf3215c03924be221993f73f2351f8ab1
Noah <Dethstroke23@gmail.com>
2022-02-10 16:14:59 +0100

    Create head.svelte

---
+++ src/components/head.svelte
@@ -0,0 +1,39 @@
+
+
+<div class="head">
+    <section class="one">
+        <h1>Hello, <strong>Joshua</strong> </h1>
+        <p>Which insurance do you want to get?</p>
+    </section>
+    <section class="two">
+        <a href="#a">
+            <i class='bx bxs-bell'></i>
+        </a>
+    </section>
+</div>
+
+<style>
+    .head {
+        display: flex;
+        justify-content: space-between;
+        padding: 1em;
+        margin-top: 30px;
+    }
+    .one h1 {
+        margin-bottom: 10px;
+        margin-top: 0;
+        font-weight: lighter;
+        font-size: 22px;
+    }
+    .one p {
+        margin: 0;
+        color: #023859;
+    }
+    .two i {
+        font-size: 30px;
+    }
+    a {
+        text-decoration: none;
+        color: black;
+    }
+</style>
