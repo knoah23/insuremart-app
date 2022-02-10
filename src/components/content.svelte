f461784f9fb61304f8dd47f0c40111bd3da450ab
Noah <Dethstroke23@gmail.com>
2022-02-10 16:14:57 +0100

    Create content.svelte

---
+++ src/components/content.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Cards from "./cards.svelte";
+</script>
+
+<div class="child">
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+    <Cards/>
+</div>
+
+<style>
+    .child {
+        display: grid;
+        grid-gap: 1rem;
+        width: 100%;
+        margin: 0 auto;
+        margin-top: 20px;
+    }
+</style>
